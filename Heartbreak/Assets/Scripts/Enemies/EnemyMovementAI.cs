53443d1e29792c838c913b897141627cf333195d
ZclipseCode <bmichaelbusiness@gmail.com>
2024-06-05 21:31:46 -0500

    enemy movement ai

---
+++ Heartbreak/Assets/Scripts/Enemies/EnemyMovementAI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementAI : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask playerLayer;
+    [SerializeField] float walkPointRange = 1f;
+    [SerializeField] float sightRange = 1f;
+    NavMeshAgent agent;
+    Transform player;
+    Vector3 walkPoint;
+    bool walkPointSet;
+    bool playerInSight;
+    // bool canMoveWhileAttacking;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+
+        if (!playerInSight)
+        {
+            Patrol();
+        }
+        else
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    void Patrol()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        else
+        {
+            agent.SetDestination(walkPoint);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+            // walkpoint reached
+            if (distanceToWalkPoint.magnitude < 1f)
+            {
+                walkPointSet = false;
+            }
+        }
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
