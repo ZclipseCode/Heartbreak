f3270b332acebae5fa1a5df8d66ca5d39090519c
ZclipseCode <bmichaelbusiness@gmail.com>
2024-06-12 17:52:27 -0500

    input actions

--- Heartbreak/Assets/Testing/Brian/TestPlayerMovement.cs
+++ Heartbreak/Assets/Testing/Brian/TestPlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewPlayerMovement : MonoBehaviour
+public class TestPlayerMovement : MonoBehaviour
 {
     [SerializeField] Transform orientation;
     [SerializeField] LayerMask ground;
